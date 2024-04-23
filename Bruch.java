590559f65915087ef08d29d0193853222038502d
langeholger <holger.la@googlemail.com>
2024-04-23 14:18:18 +0200

    Methodenaufruf S. 182

---
+++ Bruch.java
@@ -0,0 +1,10 @@
+package javamethoden;
+
+class Bruch {
+int zaehler;
+int nenner;
+
+void ausgeben() { 
+    System.out.print(" " + zaehler + "/" + nenner);
+    }
+}
---
+++ Bruchtest1.java
@@ -0,0 +1,10 @@
+package javamethoden;
+
+public class Bruchtest1 {
+public static void main(String[] args) {
+    Bruch b = new Bruch();
+    b.zaehler = 3;
+    b.nenner = 4;
+    b.ausgeben();
+}    
+}
